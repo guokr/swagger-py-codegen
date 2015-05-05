36c12bd516f505c7375634109f45482f3e2e905c
Rejown Xu <rejown@gmail.com>
2015-05-05 13:49:29 +0800

    rm example code

--- test-app/test_app/v1/api/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-import flask_restful as restful
-
-from ..validators import request_validate
-from ..filters import response_filter
-
-
-class Resource(restful.Resource):
-    method_decorators = [request_validate, response_filter]

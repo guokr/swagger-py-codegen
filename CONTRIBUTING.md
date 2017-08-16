3a3ae88a3cd0b09bcbf20e8015441074153d9dec
Fantas-LeoTao <leotao19930820@gmail.com>
2017-08-16 18:09:32 +0800

    standardize

---
+++ CONTRIBUTING.md
@@ -0,0 +1,22 @@
+# Contributing
+
+Welcome to contribute! We are following the next standards.
+
+## Installation
+
+use `virtualenv` create the develop environment:
+
+```
+cd swagger-py-codegen
+virtualenv .
+source bin/activate
+pip install -r requirements.txt
+```
+
+## Running tests
+
+use `pytest` to run the tests:
+
+```
+pytest
+```
--- setup.py
+++ setup.py
@@ -9,6 +9,9 @@ with open('swagger_py_codegen/_version.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
 
+with open('requirements.txt') as f:
+    requirements = [l for l in f.read().splitlines() if l]
+
 setup(
     name='swagger-py-codegen',
     description='Generate Flask code from Swagger docs.',
@@ -24,7 +27,7 @@ setup(
             'swagger_py_codegen=swagger_py_codegen:generate'
         ]
     },
-    install_requires=['PyYAML', 'click', 'jinja2', 'dpath', 'six'],
+    install_requires=requirements,
     tests_require=['pytest'],
     classifiers=[
         'Development Status :: 3 - Alpha',

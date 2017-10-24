00b62647d1b69ff7b2f27863004d008302baefab
maria <maria@mediaiqdigital.com>
2017-10-24 10:39:03 +0100

    modified parser.py file to handle a unicode string input in the swagger json

--- swagger_py_codegen/parser.py
+++ swagger_py_codegen/parser.py
@@ -10,7 +10,9 @@ from jsonspec.reference import resolve
 
 
 def schema_var_name(path):
-    return ''.join(map(str.capitalize, path))
+    #tuple unpacking to handle unicode
+    myTuple=[str(w) for w in path]
+    return ''.join(map(str.capitalize, myTuple))
 
 
 class RefNode(dict):
